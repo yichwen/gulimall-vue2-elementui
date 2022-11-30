e55d29706ac73640beff16f2a85193483c40719d
Lim Yi Chwen <yichwenlim@gmail.com>
2022-11-30 22:02:42 +0800

    initial commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Hello, {{ name }}</h1>
+    <el-radio v-model="radio" label="1">Selection</el-radio>
+    <el-radio v-model="radio" label="2">Selection</el-radio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hello',
+  data() {
+    return {
+      name: 'John Doe',
+      radio: '2'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
---
+++ src/components/MyTable.vue
@@ -0,0 +1,27 @@
+<template>
+  <el-table :data="tableData">
+    <el-table-column prop="date" label="Date" width="140"> </el-table-column>
+    <el-table-column prop="name" label="Name" width="120"> </el-table-column>
+    <el-table-column prop="address" label="Address"> </el-table-column>
+  </el-table>
+</template>
+
+<script>
+export default {
+  name: "MyTable",
+  data() {
+    const item = {
+      date: "2016-05-02",
+      name: "Tom",
+      address: "No. 189, Grove St, Los Angeles",
+    };
+    return {
+      tableData: Array(20).fill(item),
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
